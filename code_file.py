0896610ca5fa0ac4f9e7e602d18ba67d0cecafd5
chrisz89 <111377491+chrisz89@users.noreply.github.com>
2022-12-05 20:11:49 -0500

    Update code_file.py

--- code_file.py
+++ code_file.py
@@ -1,19 +1,52 @@
-import random
+print('Welcome to Pop Trivia!')
 
-print('Welcome to your Password Generator')
+trivia_1 = "Who is Luke Skywalker's father?"
+triv_1_ans = "darth vader"
+trivia_2 = "What character does Chris Hemsworth play in the Marvel movies?"
+triv_2_ans = "thor"
+trivia_3 = "What fictional character would you not like when he's angry?"
+triv_3_ans = "the hulk"
+trivia_4 = "What is a famous style of animation originating from Japan?"
+triv_4_ans = "anime"
+trivia_5 = "Who was a famous business reality tv star, who had a failed political career?"
+triv_5_ans = "trump"
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXXYZ~`!@#$%^&*+=:;?0123456789'
+triv_ques = [trivia_1, trivia_2, trivia_3, trivia_4, trivia_5]
+triv_ans = [triv_1_ans, triv_2_ans, triv_3_ans, triv_4_ans, triv_5_ans]
 
-number = input('Amount of passwords to generate: ')
-number = int(number)
+correct = []
+def pop_trivia():
+    count = 0
+    guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
+    while count < 4:
+        if guess.lower() in triv_ans:
+            correct.append(triv_ans[0 + count])
+            count += 1
+            triv_guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
+        elif guess.lower() == "quit":
+            print("Goodbye!")
+            print(correct)
+            break
+        else:
+            count += 1
+            guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
+    return correct
+        
+pop_trivia()
 
-length = input('Input desired password length: ')
-length = int(length)
+right_ans = 0
+incorrect = 0
+guess_counter = 0
 
-print('\nhere are your passwords: ')
+while guess_counter < 4:
+    for guess in correct:
+        range(len(correct))    
+        if guess in triv_ans:
+            guess_counter += 1
+            right_ans += 1
+        else:
+            guess_counter += 1
+            incorrect += 1
 
-for pwd in range(number):
-    passwords = ''
-    for c in range(length):
-        passwords += random.choice(chars)
-    print(passwords)
+percent =  (right_ans - 3) * 20
+print(percent, "%")
