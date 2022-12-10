b88c06b4b91424a045e588a8ef8395385a953252
chrisz89 <111377491+chrisz89@users.noreply.github.com>
2022-12-10 18:51:10 -0500

    Update code_file.py

--- code_file.py
+++ code_file.py
@@ -9,27 +9,23 @@ triv_3_ans = "the hulk"
 trivia_4 = "What is a famous style of animation originating from Japan?"
 triv_4_ans = "anime"
 trivia_5 = "Who was a famous business reality tv star, who had a failed political career?"
-triv_5_ans = "trump"
+triv_5_ans = "donald trump"
 
 triv_ques = [trivia_1, trivia_2, trivia_3, trivia_4, trivia_5]
 triv_ans = [triv_1_ans, triv_2_ans, triv_3_ans, triv_4_ans, triv_5_ans]
 
 correct = []
 def pop_trivia():
-    count = 0
-    guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
-    while count < 4:
-        if guess.lower() in triv_ans:
-            correct.append(triv_ans[0 + count])
-            count += 1
-            triv_guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
-        elif guess.lower() == "quit":
-            print("Goodbye!")
-            print(correct)
-            break
-        else:
-            count += 1
-            guess = input(triv_ques[0 + count] +  "\nEnter response here: ")
+    counter = 5
+    guess = input(triv_ques[4 - counter] +  "\nEnter response here: ")
+    while counter != 0:
+            if guess.lower() in triv_ans:
+                if guess.lower() == triv_ans[4 - counter]:
+                    correct.append(triv_ans[4 - counter])
+                    counter -= 1
+                    guess = input(triv_ques[4 - counter] +  "\nEnter response here: ")
+            else:
+                break
     return correct
         
 pop_trivia()
@@ -38,7 +34,7 @@ right_ans = 0
 incorrect = 0
 guess_counter = 0
 
-while guess_counter < 4:
+while guess_counter < 5:
     for guess in correct:
         range(len(correct))    
         if guess in triv_ans:
@@ -48,5 +44,5 @@ while guess_counter < 4:
             guess_counter += 1
             incorrect += 1
 
-percent =  (right_ans - 3) * 20
-print(percent, "%")
+percent = right_ans * 20
+print("Your score is:", percent, "%")
